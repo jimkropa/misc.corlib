41bb6d9b0b7bc338476d5f0737e254c7b9b56d0d
Jim Kropa <github@kropa.net>
2016-02-07 15:40:03 -0800

    add SymmetricTransformerFactory for IOC of DI

---
+++ src/misc.corlib/Security/Cryptography/SymmetricTransformerFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace MiscCorLib.Security.Cryptography
+{
+	using System.Security.Cryptography;
+
+	public class SymmetricTransformerFactory
+	{
+		private readonly SymmetricAlgorithm algorithm;
+
+		// For dependency injection:
+		public SymmetricTransformerFactory(
+			SymmetricAlgorithm algorithm)
+		{
+			this.algorithm = algorithm;
+		}
+
+		/*
+		public Encryptor CreateEncryptor()
+		{
+			return new Encryptor(this.algorithm, );
+		}
+		*/
+	}
+}
