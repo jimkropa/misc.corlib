86136fe2769f69f1cf41ebfc802d0b3dc0dc6495
Jim Kropa <github@kropa.net>
2016-03-13 19:35:14 -0700

    add license header to all files

--- src/misc.corlib/Security/Cryptography/AlgorithmFactory.cs
+++ src/misc.corlib/Security/Cryptography/AlgorithmFactory.cs
@@ -1,4 +1,25 @@
-﻿namespace MiscCorLib.Security.Cryptography
+﻿#region [ license and copyright boilerplate ]
+/*
+	MiscCorLib.Security.Cryptography
+	AlgorithmFactory.cs
+
+	Copyright (c) 2015 Jim Kropa (http://www.kropa.net/)
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+#endregion
+
+namespace MiscCorLib.Security.Cryptography
 {
 	// TODO: Enable IOC of injection of SymmetricAlgorithm, HashAlgorithm, and RandomNumberGenerator by non-static Factory classes.
 	public sealed class AlgorithmFactory
