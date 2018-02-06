e5f9105fb22759396a0f75fc4609052d90420d6c
KRopa <github@kropa.net>
2018-02-05 22:30:11 -0800

    create branch for Linux development environment targeting dot net standard

--- src/misc.corlib/Collections/PagingInfo.cs
+++ src/misc.corlib/Collections/PagingInfo.cs
@@ -1,32 +1,11 @@
-﻿#region [ license and copyright boilerplate ]
-/*
-	MiscCorLib.Collections
-	PagingInfo.cs
-
-	Copyright (c) 2016 Jim Kropa (https://github.com/jimkropa)
-
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
-
-		http://www.apache.org/licenses/LICENSE-2.0
-
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
-*/
-#endregion
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Runtime.Serialization;
 
 namespace MiscCorLib.Collections
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Diagnostics.CodeAnalysis;
-	using System.Diagnostics.Contracts;
-	using System.Runtime.Serialization;
-
 	/// <summary>
 	/// A simple struct with a robust set of metadata
 	/// about a page within a "paged" collection of items,
--- src/misc.corlib/IHasValue.cs
+++ src/misc.corlib/IHasValue.cs
@@ -1,28 +1,7 @@
-﻿#region [ license and copyright boilerplate ]
-/*
-	MiscCorLib
-	IHasValue.cs
-
-	Copyright (c) 2016 Jim Kropa (https://github.com/jimkropa)
-
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
-
-		http://www.apache.org/licenses/LICENSE-2.0
-
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
-*/
-#endregion
+﻿using System;
 
 namespace MiscCorLib
 {
-	using System;
-
 	/// <summary>
 	/// Similar to the <see cref="Nullable{T}.HasValue"/>
 	/// property of <see cref="Nullable{T}"/>, adds a
